905fd93973e854d2e4803885c0af413a2ee13f4e
James Y. Rauhut <jamesrauhut@us.ibm.com>
2015-12-08 15:04:36 -0600

    Card slide out animation

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,53 +1,81 @@
 import React from 'react';
+import Collapse from 'react-collapse';
 import "../sass/Card.scss";
 
 export default class Card extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {open: props.open};
+    this.state = {open: false};
+  }
+
+  handleClick = (e) => {
+    this.setState({ open: !this.state.open });
   }
 
   render() {
+
+    if (this.state.open) {
+      this.handleClick;
+    }
+
+    const toggleTileClass = this.state.open
+      ? 'card__tile card__tile--expanded'
+      : 'card__tile'
+
+    const toggleDescriptionHeight = this.state.open
+      ? {height: this.props.descriptionHeight}
+      : {height: this.props.descriptionHeight}
+
     return (
       <div className="card" style={{
-        backgroundColor: '#F2F2F2',
-        maxWidth: '450px',
-        padding: '2em',
-        position: 'relative'
-      }}>
-        <p style={{
-          color: '#999',
-          marginTop: 0
+          display: 'inline-block',
+          maxWidth: '450px',
+          position: 'relative'
         }}>
-          {this.props.date}
-        </p>
-        <img src={this.props.img} style={{
-          borderRadius: '50%',
-          height: '4em',
-          width: '4em'
-        }} />
-        <h3>{this.props.title}</h3>
-        <p style={{
-          height: '0',
-          marginBottom: 0,
-          overflow: 'hidden'
-        }}>
-          {this.props.description}
-        </p>
+        <button type="button" className={toggleTileClass}  onClick={this.handleClick} style={{
+            border: 'none',
+            padding: '2em 2em 0 2em',
+            textAlign: 'left',
+            width: '100%'
+          }}>
+          <p style={{
+              color: '#999',
+              marginTop: 0
+            }}>
+            {this.props.date}
+          </p>
+          <img src={this.props.img} style={{
+              borderRadius: '50%',
+              height: '4em',
+              width: '4em'
+            }} />
+          <h3>{this.props.title}</h3>
+          <div style={{
+                marginTop: '1em'
+              }}>
+            <Collapse isOpened={this.state.open}>
+              <p ref="description" className='card__tile__description' style={{
+                  paddingBottom: '2em'
+                }}>
+                {this.props.description}
+              </p>
+            </Collapse>
+          </div>
+        </button>
         <button type="button" className="card__pin" style={{
-          backgroundColor: 'transparent',
-          border: 'none',
-          bottom: '0',
-          padding: '0',
-          position: 'absolute',
-          right: '0'
-        }}>
+            backgroundColor: 'transparent',
+            border: 'none',
+            bottom: '0',
+            padding: '0',
+            position: 'absolute',
+            right: '0'
+          }}>
           <div className="card__pin__triangle" style={{
-            width: 0,
-            height: 0,
-            borderStyle: 'solid',
-            borderWidth: '0 0 3em 3em'
-          }} />
+              width: 0,
+              height: 0,
+              borderStyle: 'solid',
+              borderWidth: '0 0 3em 3em'
+            }} />
         </button>
       </div>
     );

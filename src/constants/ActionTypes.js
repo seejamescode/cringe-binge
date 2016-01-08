e397d82171a00864c29217a5868e42bddf8725e2
James Y. Rauhut <jamesrauhut@us.ibm.com>
2016-01-08 15:41:25 -0600

    Unnecessary actions

--- src/constants/ActionTypes.js
+++ src/constants/ActionTypes.js
@@ -1,7 +1,1 @@
 export const TOGGLE_PIN = 'TOGGLE_PIN';
-export const ADD_TODO = 'ADD_TODO';
-export const DELETE_TODO = 'DELETE_TODO';
-export const EDIT_TODO = 'EDIT_TODO';
-export const MARK_TODO = 'MARK_TODO';
-export const MARK_ALL = 'MARK_ALL';
-export const CLEAR_MARKED = 'CLEAR_MARKED';
--- src/reducers/cards.js
+++ src/reducers/cards.js
@@ -1,4 +1,4 @@
-import { TOGGLE_PIN, ADD_TODO, DELETE_TODO, EDIT_TODO, MARK_TODO, MARK_ALL, CLEAR_MARKED } from '../constants/ActionTypes';
+import { TOGGLE_PIN } from '../constants/ActionTypes';
 
 const initialState = require('../cards.json');
 initialState.forEach(function (e, id) {
@@ -13,42 +13,6 @@ export default function cards(state = initialState, action) {
         { ...card, pinned: !card.pinned } :
         card
     );
-
-  case ADD_TODO:
-    return [{
-      id: (state.length === 0) ? 0 : state[0].id + 1,
-      marked: false,
-      text: action.text
-    }, ...state];
-
-  case DELETE_TODO:
-    return state.filter(todo =>
-      todo.id !== action.id
-    );
-
-  case EDIT_TODO:
-    return state.map(todo =>
-      todo.id === action.id ?
-        { ...todo, text: action.text } :
-        todo
-    );
-
-  case MARK_TODO:
-    return state.map(todo =>
-      todo.id === action.id ?
-        { ...todo, marked: !todo.marked } :
-        todo
-    );
-
-  case MARK_ALL:
-    const areAllMarked = state.every(todo => todo.marked);
-    return state.map(todo => ({
-      ...todo,
-      marked: !areAllMarked
-    }));
-
-  case CLEAR_MARKED:
-    return state.filter(todo => todo.marked === false);
 
   default:
     return state;
